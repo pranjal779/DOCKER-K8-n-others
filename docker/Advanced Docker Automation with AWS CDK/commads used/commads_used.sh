f982c8e5db41329c1cdccb16695d5e334aad0976
Pranjal Shukla <pranjal779@gmail.com>
2023-02-11 17:12:33 -0500

    AWS docker CDK

---
+++ docker/Advanced Docker Automation with AWS CDK/commads used/commads_used.sh
@@ -0,0 +1,10 @@
+cdk init app --language=python
+
+python -m ensurepip --upgrade
+python -m pip install --upgrade pip
+python -m pip install --upgrade virtualenv
+
+
+# Pranjal1:~/environment/dockerdemo/02_02 (master) $ python -m venv 02_02
+# Pranjal1:~/environment/dockerdemo/02_02 (master) $ source 02_02/bin/activate
+# (02_02) Pranjal1:~/environment/dockerdemo/02_02 (master) $ 

076eb0a07e9955b303e93952b28798aaf4874752
Pranjal Shukla <pranjal779@gmail.com>
2024-02-19 09:11:44 -0500

    commands for K8s tutorials + Project

---
+++ kubernetes/TechWithNana/2-18-24/commands-K8s.sh
@@ -0,0 +1,62 @@
+# Create the Secret
+
+# kubectl apply -f https://k8s.io/examples/pods/inject/secret.yaml
+# View information about the Secret:
+
+# kubectl get secret test-secret
+# Output:
+
+# NAME          TYPE      DATA      AGE
+# test-secret   Opaque    2         1m
+# View more detailed information about the Secret:
+
+# kubectl describe secret test-secret
+# Output:
+
+# Name:       test-secret
+# Namespace:  default
+# Labels:     <none>
+# Annotations:    <none>
+
+# Type:   Opaque
+
+# Data
+# ====
+# password:   13 bytes
+# username:   7 bytes
+
+doskey /history # For Command Prompt
+history # For terminal
+minikube start --driver docker
+minikube status
+kubectl get node
+echo -n mongo | base64
+echo -n mongouser | base64
+minikube status
+minikube start
+minikube status
+kubectl get pod
+kubectl apply -f mongo-config.yaml
+kubectl get all
+kubectl get secret
+kubectl get pod
+kubectl --help
+kubectl describe service webapp-service
+kubectl describe pod mongo-deployment-7545677698-56t9z
+kubectl get pod
+kubectl logs mongo-deployment-7545677698-56t9z
+kubectl logs webapp-deployment-86cd4c8fb8-rmmbj
+kubectl logs webapp-deployment-86cd4c8fb8-rmmbj -f
+kubectl logs mongo-deployment-7545677698-56t9z -f
+kubectl logs webapp-deployment-86cd4c8fb8-rmmbj -f
+kubectl get svc
+minikube ip
+kubectl get node
+kubectl get node -o wide
+kubectl get svc -o wide
+kubectl stop
+kubectl --help
+minikube stop
+history
+History
+doskey /history
